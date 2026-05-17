1eeaa84eeb748d7b23970fc38eb78b5232c77fd1
ilaydayla <ilaydaisik881@gmail.com>
2026-05-17 19:33:08 +0300

    app.py guncellendi ve model baglandi

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import sqlite3
+import joblib
 
 app = Flask(__name__)
 
@@ -8,23 +9,27 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+
 @app.route('/tahmin', methods=['POST'])
 def tahmin():
     if request.method == 'POST':
+        
         gelir = float(request.form['gelir'])
         borc = float(request.form['borc'])
         kredi_gecmisi = int(request.form['kredi_gecmisi'])
         yas = int(request.form['yas'])
         
-        if gelir > (borc * 1.5) and kredi_gecmisi == 1:
-            sonuc = "risksiz"
-        else:
-            sonuc = "riskli"
-
+        # --- MODEL YÜKLEME VE TAHMİN KISMI ---
+       
+        model = joblib.load("model.pkl")
+        tahmin_dizisi = model.predict([[gelir, borc, kredi_gecmisi, yas]])
+        sonuc = tahmin_dizisi[0] 
+      
         conn = get_db_connection()
         conn.execute('''
             INSERT INTO tahminler (gelir, borc, kredi_gecmisi, yas, sonuc) 
@@ -33,6 +38,7 @@ def tahmin():
         conn.commit()
         conn.close()
 
+        
         return render_template('index.html', sonuc=sonuc)
 
 if __name__ == '__main__':
